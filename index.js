9030d2e322904011668fe87023e54008c0328103
emon-webdev <emon.webdev@gmail.com>
2024-08-27 15:18:39 +0600

    implement jwt

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const cors = require('cors')
-const port = process.env.PORT || 5000
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const port = process.env.PORT || 5000
 
 
 app.use(cors())
@@ -29,10 +30,110 @@ async function run() {
         await client.connect();
 
         // Get the database and collection on which to run the operation
+        const userCollection = client.db("food_canvas_db").collection("users");
         const menuCollection = client.db("food_canvas_db").collection("menu");
         const reviewCollection = client.db("food_canvas_db").collection("reviews");
+        const cartCollection = client.db("food_canvas_db").collection("carts")
+
+        //jwt related api
+        app.post('/jwt', async (req, res) => {
+            try {
+                const user = req.body;
+                const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+                    expiresIn: '1h'
+                });
+                res.send({ token });
+            } catch (error) {
+                console.error('Error generating JWT:', error);
+                res.status(500).send({ message: "Internal Server Error" });
+            }
+        })
+
+        // Middleware to verify JWT
+        const verifyToken = async (req, res, next) => {
+            try {
+                console.log('Inside verify token', req.headers.authorization);
+                if (!req.headers.authorization) {
+                    return res.status(401).send({ message: "Forbidden access" });
+                }
+                const token = req.headers.authorization.split(' ')[1];
+                jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+                    if (err) {
+                        return res.status(401).send({ message: "unauthorized access" });
+                    }
+                    req.decoded = decoded;
+                    next();
+                });
+            } catch (error) {
+                console.error('Error in token verification:', error);
+                res.status(500).send({ message: "Internal Server Error" });
+            }
+        };
+        //use verify admin 
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email
+            const query = {email:email}
+            const user = await userCollection.findOne(query)
+            const isAdmin = user?.role === 'admin'
+            if(!isAdmin){
+                return res.status(403).send({ message: "Forbidden access" })
+            }
+            next()
+        }
 
 
+        //users api
+        app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        });
+
+        app.get('/users/admin/:email', verifyToken, async (req, res) => {
+            const email = req.params.email
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: "unauthorized access" })
+            }
+            const query = { email: email }
+            const user = await userCollection.findOne(query)
+            let admin = false
+            if (user) {
+                admin = user?.role === 'admin'
+            }
+            res.send({ admin })
+        })
+
+
+        app.post('/users', async (req, res) => {
+            const user = req.body
+            // insert email if user doesnt existes
+            // you can do this many ways (email unique, 2. upsert 3. simple checking)
+            const query = { email: user.email }
+            const existingUser = await userCollection.findOne(query)
+            if (existingUser) {
+                return res.send({ message: 'user already exists', insertedId: null })
+            }
+            const result = await userCollection.insertOne(user)
+            res.send(result)
+        })
+
+        app.patch('/users/admin/:id', verifyAdmin, async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: new ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    role: "admin"
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc)
+            res.send(result)
+        })
+
+        app.delete('/users/:id', verifyToken, verifyAdmin, async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await userCollection.deleteOne(query)
+            res.send(result)
+        })
 
         app.get("/menu", async (req, res) => {
             const result = await menuCollection.find().toArray()
@@ -40,6 +141,28 @@ async function run() {
         })
         app.get("/review", async (req, res) => {
             const result = await reviewCollection.find().toArray()
+            res.send(result)
+        })
+
+        // carts
+        app.post('/carts', async (req, res) => {
+            const cartItem = req.body;
+            console.log(cartItem);
+            const result = await cartCollection.insertOne(cartItem)
+            res.send(result)
+        })
+
+        app.get('/carts', async (req, res) => {
+            const email = req.query.email
+            const query = { email: email }
+            const result = await cartCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await cartCollection.deleteOne(query)
             res.send(result)
         })
 
